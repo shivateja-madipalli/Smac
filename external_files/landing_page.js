48403f1b1ede5a090eb4428207e1e204740dbd98
shivateja-madipalli <madipallishivateja@gmail.com>
2016-05-16 21:32:11 -0700

    implemented functionality, have little changes, UI looks good

--- external_files/landing_page.js
+++ external_files/landing_page.js
@@ -1,27 +1,33 @@
 //landing page.js
 
+var global_previous_room_name;
+var global_current_room;
+var global_current_user;
+
+var global_current_private_room;
+
+var socket = io();
+
 jQuery(document).ready(function($) {
 
-  var socket = io();
+  //var socket = io();
   populate_empty_rooms_data();
 
-  console.log('page landed');
+  //console.log('page landed');
 
   $('#select_username').on('click', function(event){
     event.preventDefault();
     new_user_name = $('#user_Name').val();
     if(!(jQuery.isEmptyObject(new_user_name))) {
-      console.log(new_user_name);
+      //console.log(new_user_name);
       socket.emit('new_user', new_user_name);
     }
     return false;
   });
 
-  var global_current_user;
-
   socket.on('new_user_details', function(current_user) {
     if(!(jQuery.isEmptyObject(current_user))) {
-      console.log(current_user);
+      //console.log(current_user);
       global_current_user = current_user;
       all_actions_after_creating_user(current_user);
     }
@@ -33,7 +39,7 @@ jQuery(document).ready(function($) {
 
   socket.on('populate_all_user_data', function(all_Users){
     if(!(jQuery.isEmptyObject(all_Users))) {
-      console.log(all_Users);
+      //console.log(all_Users);
       //show the users with whom he current user is chatting on top
       show_all_current_Users(all_Users);
     }
@@ -47,15 +53,12 @@ jQuery(document).ready(function($) {
     }
   });
 
-  var global_previous_room_name;
-  var global_current_room;
-
   $('#roomList_select').on('change', function(e) {
     var room_Selected = $('#roomList_select').val();
 		if(room_Selected !== 'NothingSelected') {
 			if(jQuery.isEmptyObject(global_previous_room_name)) {
-				console.log('inside previous room empty');
-        console.log('global_current_user' + global_current_user);
+				//console.log('inside previous room empty');
+        //console.log('global_current_user' + global_current_user);
         var res_obj = {
 					"username" : global_current_user,
 					"room_selected" : room_Selected
@@ -66,7 +69,7 @@ jQuery(document).ready(function($) {
 				socket.emit('user_selected_room', res_obj);
 			}
 			else if(!(jQuery.isEmptyObject(global_previous_room_name)) && room_Selected !== global_previous_room_name) {
-				console.log('inside previous room NOT empty');
+				//console.log('inside previous room NOT empty');
         var res_obj = {
 					"username" : global_current_user,
 					"room_selected" : room_Selected
@@ -77,9 +80,10 @@ jQuery(document).ready(function($) {
 				socket.emit('user_selected_room', res_obj);
 			}
 			else {
-				console.log('DAMN');
+				//console.log('DAMN');
 				remove_CssClassFrom_HTML_Element('chatSpace_div', 'chatDiv_DisplayTrue_css');
 			}
+      $('#status_creating_new_room').html("");
 		}
 		else {
 			global_previous_room_name = null;
@@ -93,12 +97,12 @@ jQuery(document).ready(function($) {
   });
 
   socket.on('user_joined_a_room', function(newly_joined_user) {
-    console.log('user_joined_a_room is called');
+    //console.log('user_joined_a_room is called');
     //this will be invoked when ever a new user joins the room
     open_chat_div();
     system_message_toall_users_in_room = newly_joined_user + " has joined the room";
     html_ToBe_added = create_A_SpanToAdd_ChatMessage(null, system_message_toall_users_in_room, 'system_Messages_to_ChatRoom_css');
-    addMessages_to_MainChat('chatSpace_table_row_td1_table_tr1_td', html_ToBe_added);
+    addMessages_to_MainChat('room_chat_display', html_ToBe_added);
   });
 
   socket.on('update_rooms_count', function (all_users_count_in_current_room, room_name){
@@ -106,48 +110,122 @@ jQuery(document).ready(function($) {
     update_rooms_select_options(room_name, all_users_count_in_current_room);
   });
 
-  $('#text_Message_input').keyup(function(e) {
-		chatMessage = $('#text_Message_input').val();
+  $('#chatRoom_messageInput').keyup(function(e) {
+		chatMessage = $('#chatRoom_messageInput').val();
 		if($(this).val().length !=0) {
-			$('#text_Message_SendButton').prop( "disabled", false );
+      if(event.keyCode == 13){
+        $("#chatRoom_messageInput_SendButton").click();
+      }
+			$('#chatRoom_messageInput_SendButton').prop( "disabled", false );
 		}
 		else {
-			$('#text_Message_SendButton').prop( "disabled", true );
+			$('#chatRoom_messageInput_SendButton').prop( "disabled", true );
 		}
 	});
 
-	$('#text_Message_SendButton').on('click', function(e) {
-		// socket input is: chat_Message
-		chatMessage = $('#text_Message_input').val();
-    console.log('exec in send text msg');
-    console.log(chatMessage);
-		socket.emit('chat_Message', global_current_room, global_current_user, chatMessage);
-    $('#text_Message_input').val('');
+	$('#chatRoom_messageInput_SendButton').on('click', function(e) {
+		chatMessage = $('#chatRoom_messageInput').val();
+    //console.log('exec in send text msg');
+    //console.log(chatMessage);
+		socket.emit('chat_Message', global_current_room, global_current_user, chatMessage, false);
+    $('#chatRoom_messageInput').val('');
 	});
+
+  $('#privateChat_messageInput').keyup(function(e) {
+    chatMessage = $('#privateChat_messageInput').val();
+    if($(this).val().length !=0) {
+      $('#privateChat_messageInput_SendButton').prop( "disabled", false );
+    }
+    else {
+      $('#privateChat_messageInput_SendButton').prop( "disabled", true );
+    }
+  });
+
+  $('#privateChat_messageInput_SendButton').on('click', function(e) {
+    private_chatMessage = $('#privateChat_messageInput').val();
+    //console.log('exec in PRIVATE send text msg');
+    //console.log(private_chatMessage);
+		socket.emit('chat_Message', global_current_private_room, global_current_user, private_chatMessage, true);
+    $('#privateChat_messageInput').val('');
+  });
+
+  socket.on('load_private_chat_history_between_2Parties', function(chat_history) {
+    //CLEAR previous chat
+    $('#private_chat_display').empty();
+
+    var chat_history_keys = Object.keys(chat_history);
+    console.log('chat_history: ');
+    console.log(chat_history);
+    for(var i=0; i<chat_history_keys.length; i++) {
+      var html_ToBe_added;
+      console.log('message from history: ' + i);
+      console.log(chat_history_keys[i]);
+  		console.log(chat_history[chat_history_keys[i]]);
+      tobe_Added = i + 1;
+      console.log(("_" + tobe_Added));
+      check_for_current_user = chat_history_keys[i].replace(("_" + tobe_Added), "");
+      console.log('check_for_current_user');
+      console.log(check_for_current_user);
+      check_for_current_user = check_for_current_user.trim();
+      console.log('after trim: ');
+      console.log(check_for_current_user);
+  		if(check_for_current_user == global_current_user) {
+  				html_ToBe_added = create_A_SpanToAdd_ChatMessage(null, chat_history[chat_history_keys[i]], 'current_User_chatSpan_css');
+  		}
+  		else {
+  				html_ToBe_added = create_A_SpanToAdd_ChatMessage(check_for_current_user, chat_history[chat_history_keys[i]], 'allOther_Users_chatSpan_css');
+  		}
+  		addMessages_to_MainChat('private_chat_display', html_ToBe_added);
+    }
+  });
 
 	socket.on('chatMessage_to_WholeRoom', function(text_user, message_ToAll) {
 		var html_ToBe_added;
-		console.log('message: ' + message_ToAll);
+		//console.log('message: ' + message_ToAll);
 		if(text_user == global_current_user) {
 				html_ToBe_added = create_A_SpanToAdd_ChatMessage(null, message_ToAll, 'current_User_chatSpan_css');
 		}
 		else {
 				html_ToBe_added = create_A_SpanToAdd_ChatMessage(text_user, message_ToAll, 'allOther_Users_chatSpan_css');
 		}
-		addMessages_to_MainChat('chatSpace_table_row_td1_table_tr1_td', html_ToBe_added);
+		addMessages_to_MainChat('room_chat_display', html_ToBe_added);
 
+	});
+
+  // $('div').animate({scrollTop: $('div').get(0).scrollHeight}, 3000);
+
+  socket.on('chatMessage_to_Private_Room', function(sender_user, message_to_privateRoom) {
+		var html_ToBe_added;
+		//console.log('message: ' + message_to_privateRoom);
+		if(sender_user == global_current_user) {
+				html_ToBe_added = create_A_SpanToAdd_ChatMessage(null, message_to_privateRoom, 'current_User_chatSpan_css');
+		}
+		else {
+				html_ToBe_added = create_A_SpanToAdd_ChatMessage(sender_user, message_to_privateRoom, 'allOther_Users_chatSpan_css');
+		}
+		addMessages_to_MainChat('private_chat_display', html_ToBe_added);
 	});
 
   socket.on('user_left_the_room', function(username_who_left_the_current_room) {
     system_message_toall_users_in_room_saying_the_user_left = username_who_left_the_current_room + " has left the room";
     html_ToBe_added = create_A_SpanToAdd_ChatMessage(null, system_message_toall_users_in_room_saying_the_user_left, 'system_Messages_to_ChatRoom_css');
-    addMessages_to_MainChat('chatSpace_table_row_td1_table_tr1_td', html_ToBe_added);
+    addMessages_to_MainChat('room_chat_display', html_ToBe_added);
   });
+
+  socket.on('private_chat_initiated_between_2Parties', function(reciever_user_name, sender_user_name, private_room_name) {
+    //console.log('exec in private_chat_initiated_between_2Parties');
+    system_message_toall_private_chat_window =  "private chat started between " + sender_user_name + " and " + reciever_user_name;
+    html_ToBe_added = create_A_SpanToAdd_ChatMessage(null, system_message_toall_private_chat_window, 'system_Messages_to_ChatRoom_css');
+    addMessages_to_MainChat('private_chat_display', html_ToBe_added);
+    global_current_private_room = private_room_name;
+    //console.log('exec in private_chat_initiated_between_2Parties COMPLETED');
+  });
+
 });
 
 function all_actions_after_creating_user(current_user_name) {
-  console.log('current user name: ' + current_user_name);
-  $('#welcome_Message').html('Welcome! ' + current_user_name + ' to Chat App');
+  //console.log('current user name: ' + current_user_name);
+  $('#welcome_Message').html('Welcome! <span class="green_color_span"> ' + current_user_name + '</span>');
   $('#rooms_div').show();
   $('#userLogin_div').hide();
 }
@@ -164,7 +242,7 @@ function populate_empty_rooms_data() {
 
 //need to check if room name is already exists
 function populate_rooms_Data(rooms_data) {
-  console.log('populate_rooms_Data is called');
+  //console.log('populate_rooms_Data is called');
   if(rooms_data != null) {
     var append_to_roomList = false;
     var fragment = document.createDocumentFragment();
@@ -183,14 +261,15 @@ function populate_rooms_Data(rooms_data) {
       }
     }
     if(append_to_roomList) {
-      console.log('append_to_roomList true');
+      //console.log('append_to_roomList true');
       $('#roomList_select').append(fragment);
+      $('#status_creating_new_room').html("new room created");
     }
   }
 }
 
 function update_rooms_select_options(room_name, number_of_users_in_the_room) {
-  console.log('update_rooms_select_options is called');
+  //console.log('update_rooms_select_options is called');
   $('#roomList_select option:contains("' + room_name + '")').text(room_name + "("+number_of_users_in_the_room+")");
 }
 
@@ -210,27 +289,47 @@ function create_A_SpanToAdd_ChatMessage(user_who_sent_message, chat_Text, span_C
 
 function addMessages_to_MainChat(div_Element_Id, string_tobe_added) {
 	$('#' + div_Element_Id).append("</br></br>"+string_tobe_added);
+  updateScroll_OnChatSpace(div_Element_Id);
 }
 
 function empty_Chat_for_newRoom() {
-  $('#chatSpace_table_row_td1_table_tr1_td').html('');
+  $('#room_chat_display').html('');
 }
 
 function show_all_current_Users(all_users_in_the_App) {
-  //adding all the users in the app chatSpace_table_row_td2_css
+  //adding all the users in the app allUsers_div_css
   var allKeys = Object.keys(all_users_in_the_App);
-  var finalHtml;
-  finalHtml = "<table>";
+  var finalHtml = "<h4 id='allUsers_heading' class='room_chat_heading_css'>All Users in the App</h4>";
+  // finalHtml += "<table>";
+  // var check_for_disable_enable_privateChat_messageInput = false;
+  // for(var i=0; i < allKeys.length; i++) {
+  //   if(all_users_in_the_App[allKeys[i]] != global_current_user) {
+  //     var htmlContent = "<tr>";
+  //     htmlContent += "<td>";
+  //     htmlContent += "<button type='button' id=" + all_users_in_the_App[allKeys[i]] + " onclick = common_ClickEvent_Forall_Users(this.id) >" + all_users_in_the_App[allKeys[i]] + "</button>";
+  //     htmlContent += "</td>";
+  //     htmlContent += "</tr>";
+  //     finalHtml += htmlContent;
+  //     check_for_disable_enable_privateChat_messageInput = true;
+  //   }
+  // }
+  finalHtml += "<ul class='allusers_ul_css'>";
+  var check_for_disable_enable_privateChat_messageInput = false;
   for(var i=0; i < allKeys.length; i++) {
-    var htmlContent = "<tr>";
-    htmlContent += "<td>";
-    htmlContent += "<button type='button' id=" + allKeys[i] + ">" + all_users_in_the_App[allKeys[i]] + "</button>";
-    htmlContent += "</td>";
-    htmlContent += "</tr>";
-    finalHtml += htmlContent;
+    // if(all_users_in_the_App[allKeys[i]] != global_current_user) {
+      var htmlContent = "<li>";
+      htmlContent += "<button type='button' style='font-family: PT Sans;font-size: medium;border: 0px;border-radius:15px;' id=" + all_users_in_the_App[allKeys[i]] + " onclick = common_ClickEvent_Forall_Users(this.id) >" + all_users_in_the_App[allKeys[i]] + "</button>";
+      htmlContent += "</li>";
+      finalHtml += htmlContent;
+      check_for_disable_enable_privateChat_messageInput = true;
+    // }
   }
-  finalHtml += "</table>";
-  $('#chatSpace_table_row_td2').html(finalHtml);
+  finalHtml += "</ul>";
+  if(check_for_disable_enable_privateChat_messageInput) {
+    $('#privateChat_messageInput').prop( "disabled", false );
+  }
+  console.log(finalHtml);
+  $('#allUsers_div').html(finalHtml);
 }
 
 function remove_CssClassFrom_HTML_Element(elementName, className) {
@@ -241,8 +340,8 @@ function remove_CssClassFrom_HTML_Element(elementName, className) {
 
 function add_all_users_in_current_room(all_users_in_current_room) {
   if(all_users_in_current_room != null) {
-    //add a ul - li to 'chatSpace_table_row_td2' div
-    $('#chatSpace_table_row_td2').html("");
+    //add a ul - li to 'allUsers_div' div
+    $('#allUsers_div').html("");
     allKeys = Object.keys(all_users_in_current_room);
     var html_content = "<ul>";
       for(var i=0;i<allKeys.length;i++) {
@@ -251,6 +350,22 @@ function add_all_users_in_current_room(all_users_in_current_room) {
         html_content += "</li>";
       }
     html_content += "</ul>";
-    $('#chatSpace_table_row_td2').html(html_content);
+    $('#allUsers_div').html(html_content);
   }
+}
+
+function common_ClickEvent_Forall_Users(button_Id) {
+  //console.log("private chat opened with: ");
+  //console.log(button_Id);
+  // //console.log($('#'+button_Id).val());
+  if(button_Id != global_current_user) {
+    $('#private_chat_display').empty();
+    socket.emit('initiate_private_message', button_Id, global_current_user);
+  }
+}
+
+
+function updateScroll_OnChatSpace(div_element_Id){
+    var element = document.getElementById(div_element_Id);
+    element.scrollTop = element.scrollHeight;
 }
--- index.js
+++ index.js
@@ -31,6 +31,12 @@ var users = {};
 
 var rooms = {};
 
+var privateRooms = [];
+
+sockets = [];
+
+var saving_private_chat = {};
+
 
 // var return_for_user_signup = {
 //   "welcome_note": "Welcome! to the Chat App",
@@ -47,26 +53,24 @@ var rooms = {};
 
 io.on('connection', function(socket) {
 
-  console.log('a user connected with socket id: ' + socket.id);
+  sockets.push(socket);
+
+  //console.log('a user connected with socket id: ' + socket.id);
 
   if(Object.keys(rooms).length > 0) {
-    console.log('rooms:');
-    console.log(rooms);
-    // //io.sockets.adapter.rooms[selected_room_data.room_selected]; will give all the sockets in that particular room
-    // for(var i=0; rooms.length; i++) {
-    //   io.sockets.adapter.rooms[selected_room_data.room_selected];
-    // }
+    //console.log('rooms:');
+    //console.log(rooms);
     io.sockets.emit('rooms_data_Updates', rooms);
   }
   else {
-    console.log('rooms:');
-    console.log(rooms);
+    //console.log('rooms:');
+    //console.log(rooms);
   }
 
   socket.on('new_user', function(user_name) {
     var user_name_exists = false;
     if(Object.keys(users).length > 0) {
-      console.log('inside user length > 0: ' + users);
+      //console.log('inside user length > 0: ' + users);
       for(var i=0;i<Object.keys(users).length;i++) {
         users[Object.keys(users)[i]]
         if(users[Object.keys(users)[i]] === user_name) {
@@ -77,11 +81,11 @@ io.on('connection', function(socket) {
       }
     }
     if(!user_name_exists) {
-      console.log('new user name creating in process');
+      //console.log('new user name creating in process');
       socket.nickname = user_name;
       // users.push({id : socket.id, username : user_name});
       users[socket.id] = user_name;
-      console.log(users);
+      //console.log(users);
       socket.emit('new_user_details', users[socket.id]);
       io.sockets.emit('populate_all_user_data', users);
     }
@@ -90,50 +94,56 @@ io.on('connection', function(socket) {
   socket.on('new_room', function(room_name) {
 
     var room_name_exists = false;
-    console.log(rooms);
+    //console.log(rooms);
     var all_keys_of_rooms = Object.keys(rooms);
     if(all_keys_of_rooms.length > 0) {
       Object.keys(rooms).forEach(function(key) {
-        console.log('check');
-        console.log(rooms[key]);
+        //console.log('check');
+        //console.log(rooms[key]);
         if(rooms[key].room_name == room_name) {
-          console.log('inside room name found');
+          //console.log('inside room name found');
           room_name_exists = true;
           io.sockets.emit('rooms_data_Updates', null);
         }
       });
     }
     if(!room_name_exists) {
-      console.log('new room name creating in process');
+      //console.log('new room name creating in process');
       var json_to_save_room_data = {
         "room_name" : room_name,
         "users" : []
       };
       // rooms.push(room_name);
-      // console.log(rooms);
+      // //console.log(rooms);
       rooms[room_name] = json_to_save_room_data;
       io.sockets.emit('rooms_data_Updates', rooms);
     }
     else {
-      console.log('new room name creating NOT in process');
+      //console.log('new room name creating NOT in process');
     }
   });
 
   //io to be used to send messages to all users
   //socket to the particular user
 
-  socket.on('chat_Message', function(room_name, current_User, text_Msg) {
-    console.log('message: ' + text_Msg);
-    console.log('current user: ' + current_User);
-    // var replyJson = {
-    //   'current_User' : current_User,
-    //   'message_ToAll' : text_Msg
-    // }
-    io.to(room_name).emit('chatMessage_to_WholeRoom', current_User, text_Msg);
+  socket.on('chat_Message', function(room_name, current_User, text_Msg, privateOrNot) {
+    if(privateOrNot) {
+      io.to(room_name).emit('chatMessage_to_Private_Room', current_User, text_Msg);
+      // saving_private_chat[room_name].push(current_User + ": " + text_Msg);
+      console.log('saving data into private chat room:');
+      //finding count
+      count = Object.keys(saving_private_chat[room_name]).length;
+      keyVal = current_User + "_" + (parseInt(count) + 1);
+      saving_private_chat[room_name][keyVal] = text_Msg;
+      console.log(saving_private_chat);
+    }
+    else{
+      io.to(room_name).emit('chatMessage_to_WholeRoom', current_User, text_Msg);
+    }
   });
 
   socket.on('user_selected_room', function(selected_room_data) {
-    console.log(selected_room_data);
+    //console.log(selected_room_data);
     socket.join(selected_room_data.room_selected);
 
     //get all the users under this room
@@ -144,7 +154,7 @@ io.on('connection', function(socket) {
 
     users_count_in_current_room = rooms[selected_room_data.room_selected].users.length;
 
-    console.log(users_count_in_current_room);
+    //console.log(users_count_in_current_room);
 
     io.to(selected_room_data.room_selected).emit('user_joined_a_room', selected_room_data.username);
     io.sockets.emit('update_rooms_count',users_count_in_current_room, selected_room_data.room_selected);
@@ -156,12 +166,63 @@ io.on('connection', function(socket) {
     }
   });
 
+  socket.on('initiate_private_message', function(receiver_name) {
+    //private chat open
+    //get socket id with user name
+    var receiver_id;
+    Object.keys(users).forEach(function(key) {
+      if(users[key] == receiver_name) {
+        receiver_id = key;
+      }
+    });
+    current_login_user = users[socket.id];
+
+    var room_name_for_private_chatting = "privateRoom_" + current_login_user + "_And_" + receiver_name + "_";
+    console.log('room_name_for_private_chatting: ');
+    console.log(room_name_for_private_chatting);
+
+    //check if private chat have been already initiated
+    var privateRoomFound = false;
+    var all_keys_of_private_rooms = Object.keys(saving_private_chat);
+    if(all_keys_of_private_rooms.length > 0) {
+      //console.log('inside all_keys_of_private_rooms length > 0');
+
+      all_keys_of_private_rooms.forEach(function(key){
+        if(key.indexOf("_" + current_login_user + "_") != -1 && key.indexOf("_" + receiver_name + "_") != -1) {
+          privateRoomFound = true;
+        }
+      });
+
+      if(privateRoomFound) {
+        var all_chat_history = saving_private_chat[room_name_for_private_chatting];
+        console.log('saving_private_chat AGAIN:');
+        console.log(saving_private_chat);
+        console.log('inside privateRoomFound is true');
+        console.log(all_chat_history);
+        io.to(room_name_for_private_chatting).emit('load_private_chat_history_between_2Parties', all_chat_history);
+      }
+      else {
+        console.log('inside privateRoomFound is FALSE');
+      }
+    }
+    if(!privateRoomFound || all_keys_of_private_rooms.length == 0) {
+      console.log('inside all_keys_of_private_rooms NOT found');
+      privateRooms.push[room_name_for_private_chatting];
+      socket.join(room_name_for_private_chatting);
+      // //console.log(sockets);
+      io.sockets.connected[receiver_id].join(room_name_for_private_chatting);
+      io.to(room_name_for_private_chatting).emit('private_chat_initiated_between_2Parties', receiver_name, current_login_user, room_name_for_private_chatting);
+      saving_private_chat[room_name_for_private_chatting] = {};
+    }
+
+  });
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    //console.log('user disconnected');
     var users_of_room;
     Object.keys(rooms).forEach(function(key) {
       users_of_room = rooms[key].users;
-      console.log(users_of_room);
+      //console.log(users_of_room);
       index_of_socket_in_rooms = users_of_room.indexOf(socket.id);
       if(index_of_socket_in_rooms != -1) {
         username_of_the_socket = users[socket.id];
@@ -171,10 +232,10 @@ io.on('connection', function(socket) {
         users_of_room.splice(index_of_socket_in_rooms, 1);
         rooms[key].users = users_of_room;
         changed_users_count_in_current_room = users_of_room.length;
-        // console.log('###########');
-        // console.log(rooms);
-        // console.log(changed_users_count_in_current_room);
-        // console.log('###########');
+        // //console.log('###########');
+        // //console.log(rooms);
+        // //console.log(changed_users_count_in_current_room);
+        // //console.log('###########');
         io.sockets.emit('update_rooms_count',changed_users_count_in_current_room, key);
       }
     });
